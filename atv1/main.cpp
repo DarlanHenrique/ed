0ec9bef00d6b27b3a107026cdc8709bdd704b8f7
ICE <ice@lab04m35.nrc.ice.ufjf.br>
2022-05-02 20:16:39 -0300

    Aula 02.05.22

---
+++ atv1/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+//ATIVIDADE 02
+//void troca(int *a, int *b){
+    //int aux = 0;
+    //cout << "Digite a variavel A: ";
+    //cin >> *a;
+    //cout << endl << "Digite a variavel B: ";
+    //cin >> *b;
+    //aux = *b;
+    //*b = *a;
+    //*a = aux;
+    //cout << "O valor de A e: " << *a << endl << "O valor de B e: " << *b ;
+//}
+//ATIVIDADE 02
+
+//ATIVIDADE 03
+//void divisao(int num, int div, int *q, int *r){
+    //cout << "Digite um valor para num: ";
+    //cin >> num;
+    //cout << "Digite um valor para div: ";
+    //cin >> div;
+    //*q = num/div;
+    //*r = num%div;
+    //cout << "O dividendo e: " << num << endl << "O divisor e: " << div << endl << "O quociente e: " << *q << endl << "O resto e: " << *r << endl;
+//}
+//ATIVIDADE 03
+int main()
+{
+//INICIO ATIVIDADE 01
+    //int *pt =0,x;
+    //cout << "O endereco de pt e: " << &pt << endl << "Digite um valor para x: ";
+    //cin >> x;
+    //pt = &x;
+    //cout << "O conteudo da variavel pt e: " << pt << endl << "O endereco de x e: " << &x << endl;
+    //*pt = *pt*10;
+    //cout << "O conteudo da apontado pt e: " << *pt << endl;
+    //*pt = *pt +10;
+    //cout << "O conteudo da apontado pt e: " << *pt << endl << "O conteudo da variavel pt e: " << pt << endl;
+//FIM ATIVIDADE 01
+
+//INICIO ATIVIDADE 02
+    //int *a = new int, *b = new int;
+    //troca(a,b);
+    //delete a;
+    //delete b;
+//FIM ATIVIDADE 02
+
+//INICIO ATIVIDADE 03
+    //int num = 0, div = 0, *q = new int, *r = new int;
+    //divisao(num, div, q, r);
+//FIM ATIVIDADE 03
+
+    return 0;
+}
